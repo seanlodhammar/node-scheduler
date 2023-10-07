cd2a3520b970bd99af7a9c946b6ededb85888cab
Sean Lodhammar <88111973+seanlodhammar@users.noreply.github.com>
2023-10-07 17:59:54 +0100

    Deleted dist directory

--- dist/tests/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../index");
-var calendar = new index_1.Calendar('us');
-var calendarGetDateTest = calendar.getDate('21/1/2024');
-console.log(calendarGetDateTest);
