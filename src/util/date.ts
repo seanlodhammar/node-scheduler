54a64013bdec0a5ddea426fac354d3fb40e71481
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-07 21:41:38 +0100

    Added separateDateAndParse

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,48 +1,29 @@
-import { getAllMonths, days, isLeapYear } from "./util";
+import { getAllMonths, days, isLeapYear, separateDateAndParse, DatesOrFalse } from "./util";
 
-export const getDate = (date: string, config: 'eu' | 'us') => {
-    const split = date.split('/');
-    const dateArr : number[] = [];
-    split.forEach((element, _) => {
-        const elInt = parseInt(element);
-        if(!elInt || isNaN(elInt)) {
-            return false;
-        }
-        dateArr.push(elInt);
-    })
-    if(dateArr.length < 3 || dateArr.length > 3) {
-        return false;
-    }
-    if(dateArr.length === 3) {
-        const day = dateArr[0];
-        const month = dateArr[1];
-        const year = dateArr[2];
-
-    if(month > 12 || month < 1) {
-        return false;
-    }
-    
-    if(year.toString().length <= 1 || year.toString().length > 4) {
-        return false;
-    }
-    let dayStr = day.toString();
-    let monthStr = month.toString();
-    if(day.toString().length === 1) {
-        dayStr = `0${day}`;
+export interface GetDate {
+    month: {
+        name: string;
+        date: number
     };
-    if(month.toString().length === 1) {
-        monthStr = `0${month}`;
-    }
+    day: {
+        name: string;
+        date: number;
+    };
+    year: number;
+    date: Date;
+    dateStr: string;
+    leap: boolean;
+    items?: object
+}
 
-    let param = '';
-    const yearStr = `${year.toString().length === 4 ? year : ''}${year.toString().length === 2 ? `20${year}`: ''}`;
-    if(config === 'eu') {
-        param = `${yearStr}-${monthStr}-${dayStr}T00:00:00`;
-    } else if(config === 'us') {
-        param = `${yearStr}-${dayStr}-${monthStr}`;
-    } else {
-        return false;
-    }
+export const getDate = (date: string | Date, config: 'eu' | 'us'): GetDate | false => {
+    const dates = separateDateAndParse(`${date instanceof Date ? date.toLocaleString() : date}`, config);
+
+    if(!dates) return false;
+
+    const { day, month, year } = dates;
+
+    const param = `${year.str}-${month.str}-${day.str}T00:00:00`;;
 
     const data = new Date(param);
     const isValid = Date.parse(param);
@@ -51,7 +32,7 @@ export const getDate = (date: string, config: 'eu' | 'us') => {
         return false;
     }
 
-    const months = getAllMonths(yearStr);
+    const months = getAllMonths(year.str);
 
     const dayDate = data.getDate();
     const dayName = days[data.getDay()];
@@ -62,10 +43,9 @@ export const getDate = (date: string, config: 'eu' | 'us') => {
     const yearDateStr = yearDate.toString();
     const localeDateStr = data.toLocaleDateString();
 
-    if(month !== monthDate + 1) {
+    if(month.int !== monthDate + 1) {
         return false;
     }
 
     return { month: { name: monthName, date: monthDate + 1 }, day: { name: dayName, date: dayDate }, year: yearDate, date: data, dateStr: localeDateStr, leap: isLeapYear(yearDateStr) };
-}
 }
