3688eb088f5e59ec5cd13cf4644f6c4d35a18a96
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-07 21:41:00 +0100

    Added separateDateAndParse function for easier date separation

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,21 @@
+interface Dates {
+    day: {
+        str: string;
+        int: number;
+    }
+    month: {
+        str: string;
+        int: number;
+    }
+    year: {
+        str: string;
+        int: number;
+    };
+    dateString: string;
+}
+
+export type DatesOrFalse = Dates | false;
+
 export const isLeapYear = (year?: string): boolean =>  {
     const yearStr = year || '';
     const digits = parseInt(new Date(yearStr).getFullYear().toString().slice(2));
@@ -14,6 +32,72 @@ export const getAllMonths = (year?: string) => {
     }
     const months = [{ name: 'January', days: 31 }, { name: 'February', days: isLeapYear(yearStr) ? 29 : 28 }, { name: 'March', days: 31 }, { name:'April', days: 30 }, { name:'May', days: 31 }, { name:'June', days: 30 }, { name: 'July', days: 31 }, { name: 'August', days: 31 }, { name: 'September', days: 30 }, { name:'October', days: 31 }, { name: 'November', days: 30 }, { name: 'December', days: 31 }];
     return months;
+}
+
+// date in form of 10/12/23 or 10/12/2023
+export const separateDateAndParse = (date: string, config?: 'eu' | 'us' | 'irrelevant'): DatesOrFalse => {
+    const split = date.split('/');
+    const dateArr : { int: number; str: string; }[] = [];
+    split.forEach((element, _) => {
+        const elInt = parseInt(element);
+        if(!elInt || isNaN(elInt)) {
+            return false;
+        }
+        dateArr.push({ int: elInt, str: element });
+    });
+    if(dateArr.length < 3 || dateArr.length > 3) {
+        return false;
+    }
+
+    const months = getAllMonths();
+    let day;
+    let month;
+    let year;
+    
+    if(config === 'eu' || config === 'irrelevant') {
+        day = dateArr[0].int;
+        month = dateArr[1].int;
+        year = dateArr[2].int;
+    }
+    if(config === 'us') {
+        month = dateArr[0].int;
+        day = dateArr[1].int;
+        year = dateArr[2].int;
+    }
+
+    if(!month || !day || !year) {
+        return false;
+    }
+
+    const monthObj = months[month - 1];
+    if(!monthObj || month > 12 || month < 1) return false;
+    if(monthObj.days < day || day < 1) return false;
+
+    let dayStr = day.toString();
+    let monthStr = month.toString();
+
+    if(dayStr.length === 1) {
+        dayStr = `0${day}`;
+    }
+
+    if(monthStr.length === 1) {
+        monthStr = `0${month}`;
+    };
+
+    let yearStr = dateArr[2].str;
+
+    if(yearStr.length === 2) {
+        const strPrefix = new Date().getFullYear().toString().slice(0, 2);
+        yearStr = `${strPrefix}${yearStr}`;
+    }
+
+    if(yearStr.length !== 4) {
+        return false;
+    }
+
+    const dateString = `${day}/${month}/${yearStr}`;
+
+    return { day: { str: dayStr, int: day }, month: { str: monthStr, int: month }, year: { str: yearStr, int: parseInt(yearStr) }, dateString: dateString };
 }
 
 export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
