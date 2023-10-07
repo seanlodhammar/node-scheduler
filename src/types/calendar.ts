0f2a9e6c8bd9000c5588a1b1974a55a786467bc9
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-07 21:39:00 +0100

    Added types directory

---
+++ src/types/calendar.ts
@@ -0,0 +1,42 @@
+// export interface CalendarObj {
+//     [years: string]: {
+//         [months: string]: {
+//             [days: string]: CalendarItem[]
+//         }
+//     }
+// }
+
+export interface CalendarObj {
+    [dates: string]: {
+        [times: string]: CalendarItem
+    }
+}
+
+export interface CalendarItem {
+    id: string;
+    dateStr: string;
+    date: Date;
+    data: string | number | {
+        [props: string]: any;
+    };
+    duration?: {
+        hours: number;
+        minutes: number;
+    }
+    time?: {
+        str: string;
+        hour: number;
+        minute: number;
+    }
+    startTime?: {
+        str: string;
+        hour: number;
+        minute: number
+    }
+    endTime?: {
+        str: string;
+        hour: number;
+        minute: number;
+    }
+}
+
