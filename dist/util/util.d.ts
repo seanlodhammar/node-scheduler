fb327dc3308cbe610db2f3ab4477e0007a2b5d8c
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-07 22:14:27 +0100

    Removed dist dir

--- dist/util/util.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const isLeapYear: (year?: string) => boolean;
-export declare const getAllMonths: (year?: string) => {
-    name: string;
-    days: number;
-}[];
-export declare const separateDateAndParse: (date: string) => number[];
-export declare const days: string[];
