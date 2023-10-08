b4ea2645d80ab5ac1b5844b9a45028a3ac8997c9
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-08 03:21:31 +0100

    Modified script.ts

--- src/tests/script.ts
+++ src/tests/script.ts
@@ -2,7 +2,13 @@ import { Calendar } from "../index";
 
 const calendar = new Calendar('eu');
 
-const setItem = calendar.setItem('12/12/2023', { name: 'Put up Christmas Tree', description: 'Need to put up christmas tree in living room' });
+const setItemTime = calendar.setItem('18/13', 'Something I need to do', { time: '01:08' })
+calendar.setItem('13/12/2023', 'Something else I need to do')
 const getCurrentDate = calendar.getDate();
-const getOtherDate = calendar.getDate('12/12/2023');
-console.log(setItem);
+
+const getItem1Date = calendar.getDate('12/12/2023');
+const getOtherDate = calendar.getDate('13/12/2023');
+
+// console.log(setItemTime);
+
+console.log(getCurrentDate);
