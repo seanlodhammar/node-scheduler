756b390ac1483214681acc2af9d7d216e7860290
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-12 22:04:01 +0100

    Modified script.ts

--- src/tests/script.ts
+++ src/tests/script.ts
@@ -3,7 +3,10 @@ import { Calendar } from "../index";
 const calendar1 = new Calendar('eu');
 
 calendar1.setItem('25/12/23', 'Christmas', { time: '12:30' });
+calendar1.setItem('25/12/23', 'Something else', { time: '12:30' })
 const calendarObj = calendar1.get;
 
 const calendar2 = new Calendar('eu');
+// calendarObj['items']['25/12/2023']['12:30']['time']['hour'] = calendarObj['items']['25/12/2023']['12:30']['time']['hour'].toString()!;
+
 calendar2.register(calendarObj);
