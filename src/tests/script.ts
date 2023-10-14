16a0a2e1cb4bf82731053856b36d4e32853440ff
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-14 18:09:01 +0100

    Modified `package.json`

--- src/tests/script.ts
+++ src/tests/script.ts
@@ -1,9 +1,4 @@
 import { Calendar } from "../index";
 
-const calendar1 = new Calendar('eu');
+const calendar = new Calendar('eu');
 
-calendar1.setItem('25/12/23', 'Christmas', { time: '12:30' });
-// calendar1.setItem('25/12/23', 'Something else', { time: '12:30' })
-const calendarObj = calendar1.get as { [props: string]: any };
-
-const calendar2 = new Calendar('eu');
