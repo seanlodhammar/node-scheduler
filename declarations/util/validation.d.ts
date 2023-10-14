3c859e765fd879bb8c47b3f432ed285de2ac1e45
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-14 19:28:21 +0100

    Modified declarations

--- declarations/util/validation.d.ts
+++
@@ -1,6 +0,0 @@
-import { CalendarItem } from "../types/calendar";
-interface Obj {
-    [props: string | symbol | number]: any;
-}
-export declare const validateDate: (item: Obj | Obj[]) => CalendarItem | CalendarItem[];
-export {};
