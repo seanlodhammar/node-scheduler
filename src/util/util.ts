37a023ff354a3bbf97f03fe0c671c7574a292d19
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-14 19:26:06 +0100

    Added typings and moved `sanitizeCalendar` to separate file

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,22 +1,4 @@
-import { CalendarItem, CalendarItemTimeProps, CalendarObj } from "../types/calendar";
-import { validateDate } from "./validation";
-import { unsafePropError } from "../errors/validation";
-
-interface Dates {
-    day: {
-        str: string;
-        int: number;
-    }
-    month: {
-        str: string;
-        int: number;
-    }
-    year: {
-        str: string;
-        int: number;
-    };
-    dateString: string;
-}
+import { Dates } from "../types/util";
 
 export type DatesOrFalse = Dates | false;
 
@@ -141,65 +123,3 @@ export const separateDateAndParse = (date: string | Date, config: 'eu' | 'us'): 
 }
 
 export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-export const sanitizeCalendar = (calendar: { [props: string | number | symbol]: any }): CalendarObj => {
-    const sanitizedCalendar : CalendarObj = {};
-    
-    if(!('items' in calendar)) throw unsafePropError;
-
-    const dateKeys = Object.keys(calendar.items);
-
-    if(dateKeys.length < 1) {
-        return {};
-    }
-    for(let dateKey of dateKeys) {
-        const dateObj = calendar['items'][dateKey];
-        const dateTimeKeys = Object.keys(dateObj);
-        if(sanitizedCalendar[dateKey] === undefined || sanitizedCalendar[dateKey] === null) {
-            Object.defineProperty(sanitizedCalendar, dateKey, {
-               value: {},
-               configurable: true,
-               enumerable: true,
-               writable: true,
-            });
-        }
-
-        for(let dateTimeKey of dateTimeKeys) {
-            const dateTime = dateObj[dateTimeKey];
-            if(!dateTime) throw unsafePropError;
-            
-            const item = validateDate(dateTime);
-            if(sanitizedCalendar[dateKey][dateTimeKey] === undefined || sanitizedCalendar[dateKey][dateTimeKey] === null) {
-                Object.defineProperty(sanitizedCalendar[dateKey], dateTimeKey, {
-                    value: item,
-                    configurable: true,
-                    writable: true,
-                    enumerable: true,
-                })
-                continue;
-            } else if(sanitizedCalendar[dateKey][dateTimeKey]) {
-                const currentTimeObj = sanitizedCalendar[dateKey][dateTimeKey];
-                if(!Array.isArray(currentTimeObj) && typeof currentTimeObj === 'object') {
-                    Object.defineProperty(sanitizedCalendar[dateKey], dateTimeKey, {
-                        value: [item, currentTimeObj],
-                        configurable: true,
-                        writable: true,
-                        enumerable: true,
-                    });
-                    continue;
-                } else if(Array.isArray(currentTimeObj)) {
-                    Object.defineProperty(sanitizedCalendar[dateKey], dateTimeKey, {
-                        value: [item, ...currentTimeObj]
-                    })
-                    continue;
-                } else {
-                    continue;
-                }
-            } else {
-                continue;
-            }
-
-        }
-    };
-    return sanitizedCalendar;
-}
