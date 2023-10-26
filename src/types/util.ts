095e9627dcb1093950d447613612fafbbe480cde
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-26 05:21:25 +0100

    Added types

---
+++ src/types/util.ts
@@ -0,0 +1,15 @@
+export interface Dates {
+    day: {
+        str: string;
+        int: number;
+    }
+    month: {
+        str: string;
+        int: number;
+    }
+    year: {
+        str: string;
+        int: number;
+    };
+    dateString: string;
+}
