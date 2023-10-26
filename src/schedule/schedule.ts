48426b55bcf302150af27756bc9b6f409c37d07d
seanlodhammar <seanlodhammar@hotmail.com>
2023-10-26 15:34:19 +0100

    Modified `schedule.ts`

--- src/schedule/schedule.ts
+++ src/schedule/schedule.ts
@@ -1,4 +1,5 @@
-import { ScheduleObj, ScheduleItem } from "../types/schedule";
+import { ScheduleObj } from "../types/schedule";
+import { StructureItem } from "../types/general";
 
 class Schedule {
     private schedule : ScheduleObj | undefined;
